70deb59bf0f8f6b70e793a82caba4eda2f4a54c6
Sai Krishna <saikrishna100797@gmail.com>
2020-06-03 15:09:14 +0530

    Updates

---
+++ precache-manifest.af82219a3ba320509d28df9953b37d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cb96ef8bb4352481d7fa92dc0dcd8d",
+    "url": "/reddit-blog/index.html"
+  },
+  {
+    "revision": "326fc9405f98e6f9804b",
+    "url": "/reddit-blog/static/js/2.fb48cd63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-blog/static/js/2.fb48cd63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9a3439a07259892d1a",
+    "url": "/reddit-blog/static/js/main.ee16dfd3.chunk.js"
+  },
+  {
+    "revision": "917ef0bfcea6448bf575",
+    "url": "/reddit-blog/static/js/runtime-main.c3b20b00.js"
+  }
+]);
